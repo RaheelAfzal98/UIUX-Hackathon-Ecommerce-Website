b785dc009dfc95581f5b0345aff94cd512efda23
RaheelAfzal98 <infotime8123@gmail.com>
2024-12-12 10:51:57 +0500

    upload files

---
+++ ecommerce-uiux/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-center w-[1365px] h-[50px] bg-[#FBEBB5]'>
+      <ul className='flex mt-3  gap-4'>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Header
